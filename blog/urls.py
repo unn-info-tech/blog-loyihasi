f669f55423780b64192f0e806c3da78b7ef16df2
Nematulloh <unn.info.tech@gmail.com>
2026-03-27 17:30:59 +0500

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.bosh_sahifa, name='bosh_sahifa'),
+    path('post/<int:post_id>/', views.post_batafsil, name='post_batafsil'),
+    path('ommabop/', views.ommabop_postlar, name='ommabop'),
+    path('biz-haqimizda/', views.biz_haqimizda, name='biz_haqimizda'),
+    path('aloqa/', views.aloqa, name='aloqa'),
+    path('portfolio/', views.portfolio, name='portfolio'),  # Yangi
+    path('yangi/', views.post_yaratish, name='post_yaratish'),
+    path('post/<int:post_id>/izoh/', views.post_izoh, name='post_izoh'),
+    path('post/<int:post_id>/tahrirlash/', views.post_tahrirlash, name='post_tahrirlash'),
+    path('post/<int:post_id>/ochirish/', views.post_ochirish, name='post_ochirish'),
+    path('royxatdan-otish/', views.royxatdan_otish, name='royxatdan_otish'),
+    path('profil/tahrirlash/', views.profil_tahrirlash, name='profil_tahrirlash'),
+    path('profil/<str:username>/', views.profil, name='profil'),
+    path('kirish/', views.kirish, name='kirish'),
+    path('chiqish/', views.chiqish, name='chiqish'),
+
+]
